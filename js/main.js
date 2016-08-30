2c4f1e4da1f7764f89f2e1bebfe5ce221fa3aa99
Steven Leung <DevS23ven@DevS23ven.local>
2016-08-30 14:35:13 -0700

    banner

--- js/main.js
+++ js/main.js
@@ -10,9 +10,37 @@ function GSW (name, size, color, stock, price, image, magnifyImage) {
 
 var stocksArray = []
 
-var kdjacket = new GSW ('Kevin Durant 35 Wool Jacket', 'All Size', 'Royal / White', 'Limited', 500, 'img/kd/kevin-durant-35.png', 'img/kd/kevin-durant-35-2.png')
+var curryJacket = new GSW ('Stephen Curry #30 Jacket', 'All Size', 'Blue', 'Sold Out', 500, 'img/curry/curry-jacket.png', 'img/curry/curry-jacket-2.png')
+var curryJersey = new GSW ('Stephen Curry #30 Jersey', 'Small', 'Blue', '15', 110, 'img/curry/curry-jersey.png', 'img/curry/curry-jersey-2.png')
+var curryTee = new GSW ('Stephen Curry #30 Tee', 'M L', 'White', 'Limited', 20, 'img/curry/curry-tee.png', 'img/curry/curry-tee-2.png')
+var kdJacket = new GSW ('Kevin Durant #35 Jacket', 'All Size', 'Royal / White', 'Limited', 500, 'img/kd/kevin-durant-35.png', 'img/kd/kevin-durant-35-2.png')
+var kdJersey = new GSW ('Kevin Durant #35 Jersey', 'S M', 'White', '500', 110, 'img/kd/kevin-durant-jersey.png', 'img/kd/kevin-durant-jersey-2.png')
+var kdTee = new GSW ('Kevin Durant #35 Tee', 'All Size', 'Blue', 'Sold Out', 28, 'img/kd/kevin-durant-tee.png', 'img/kd/kevin-durant-tee-2.png')
+var kThomWest = new GSW ('Klay Thompson #11 West', 'Large', 'Red', 'Limited', 28, 'img/kt/kthom-west.png', 'img/kt/kthom-west-2.png')
+var kThomJersey = new GSW ('Klay Thompson #11 Jersey', 'All Size', 'Grey', 500, 110, 'img/kt/kthom-jersey.png', 'img/kt/kthom-jersey-2.png')
+var kThomTee = new GSW ('Klay Thompson #11 Tee', 'X-Large', 'Black', 500, 20, 'img/kt/kthom-tee.png', 'img/kt/kthom-tee-2.png')
+var greenUSA = new GSW ('Draymond Green Nike USA', 'All Size', 'Blue', 350, 35, 'img/green/green-usa.png', 'img/green/green-usa-2.png')
+var greenJersey = new GSW ('Draymond Green #23 Jersey', 'All Size', 'Blue', 350, 110, 'img/green/green-jersey.png', 'img/green/green-jersey-2.png')
+var greenTee = new GSW ('Draymond Green #23 Tee', 'All Size', 'Grey', 500, 28, 'img/green/green-tee.png', 'img/green/green-tee-2.png')
+var curryHead = new GSW ('Stephen Curry #30 FLATHLETE 5” FIGURINE', 'One Size', '', 'Limited', 20, 'img/head/curry-head.png', 'img/head/curry-head.png')
+var greenHead = new GSW ('Draymond Green #23 FLATHLETE 5” FIGURINE', 'One Size', '', 'Limited', 20, 'img/head/green-head.png', 'img/head/green-head.png')
+var kThomHead = new GSW ('Klay Thompson #11 FLATHLETE 5” FIGURINE', 'One Size', '', 'Limited', 20, 'img/head/kthom-head.png', 'img/head/kthom-head.png')
 
-stocksArray.push(kdjacket)
+stocksArray.push(curryJacket)
+stocksArray.push(curryJersey)
+stocksArray.push(curryTee)
+stocksArray.push(kdJacket)
+stocksArray.push(kdJersey)
+stocksArray.push(kdTee)
+stocksArray.push(kThomWest)
+stocksArray.push(kThomJersey)
+stocksArray.push(kThomTee)
+stocksArray.push(greenUSA)
+stocksArray.push(greenJersey)
+stocksArray.push(greenTee)
+stocksArray.push(curryHead)
+stocksArray.push(greenHead)
+stocksArray.push(kThomHead)
 
 for( i = 0; i < stocksArray.length; i++ ) {
 	var gswName = document.createTextNode(stocksArray[i].name)
@@ -20,34 +48,37 @@ for( i = 0; i < stocksArray.length; i++ ) {
 	var gswColor = document.createTextNode('Color: ' + stocksArray[i].color)
 	var gswStock = document.createTextNode('In stock: ' + stocksArray[i].stock)
 	var gswPrice = document.createTextNode('Price: $' + stocksArray[i].price)
-	var gswButton = document.createTextNode('Buy Now!')
+	var gswButton = document.createTextNode('ADD TO CART')
 	var gswImage = stocksArray[i].image
 	var gswMagnifyImage = stocksArray[i].magnifyImage
 
 	var newCol = document.createElement('div')
 	var newDiv = document.createElement('div')
-	var nameH1 = document.createElement('h1')
+	var nameH2 = document.createElement('h2')
 	var sizeH4 = document.createElement('h4')
 	var colorH4 = document.createElement('h4')
 	var stockH4 = document.createElement('h4')
 	var priceH4 = document.createElement('h4')
-	var buyNow = document.createElement('button')
+	var buyNow = document.createElement('a')
 	var image = document.createElement('img')
 
-	nameH1.appendChild(gswName)
+	nameH2.appendChild(gswName)
 	sizeH4.appendChild(gswSize)
 	colorH4.appendChild(gswColor)
 	stockH4.appendChild(gswStock)
 	priceH4.appendChild(gswPrice)
 	buyNow.appendChild(gswButton)
-	buyNow.className = 'btn btn-primary btn-lg'
+	buyNow.setAttribute('href', '#' + [i])
+	buyNow.setAttribute('data-price', stocksArray[i].price)
+	buyNow.className = 'cd-add-to-cart'
+
 	image.src = gswImage
 	image.setAttribute('data-magnify-src', gswMagnifyImage)
 	image.className = 'zoom img-responsive'
 
 	newCol.className = 'col-sm-4'
 	newDiv.className = 'gsw stock' + i + ' thumbnamil'
-	newDiv.appendChild(nameH1)
+	newDiv.appendChild(nameH2)
 	newDiv.appendChild(image)
 	newDiv.appendChild(sizeH4)
 	newDiv.appendChild(colorH4)
@@ -62,18 +93,6 @@ for( i = 0; i < stocksArray.length; i++ ) {
 $(document).ready(function(){
 	$('.zoom').magnify();
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
